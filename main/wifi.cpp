bf61542480e90bb710b60123515190e55940adc6
rustyx <me@rustyx.org>
2021-06-15 13:46:49 +0200

    esp-idf 4.3 support. fixes #2

--- main/wifi.cpp
+++ main/wifi.cpp
@@ -7,6 +7,11 @@ std::string wifiSsid, wifiPw;
 static EventGroupHandle_t wifi_event_group;
 const int WIFI_CONNECTED_BIT = BIT0;
 
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 3, 0)
+#define WIFI_IF_STA ESP_IF_WIFI_STA
+#define WIFI_IF_AP  ESP_IF_WIFI_AP
+#endif
+
 static void event_handler_ap(void* arg, esp_event_base_t base, int32_t event_id, void* data) {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*)data;
@@ -22,11 +27,14 @@ static void wifi_init_softap() {
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler_ap, NULL, NULL));
-    wifi_config_t wifi_config = {.ap = {{}, {}, 0, 0, wifiPw.empty() ? WIFI_AUTH_OPEN : WIFI_AUTH_WPA2_PSK, 0, 32, 300}};
-    strncpy((char*)wifi_config.ap.ssid, wifiSsid.c_str(), sizeof(wifi_config.ap.ssid) - 1);
-    strncpy((char*)wifi_config.ap.password, wifiPw.c_str(), sizeof(wifi_config.ap.password) - 1);
+    wifi_config_t wificfg = {};
+    wificfg.ap.authmode = wifiPw.empty() ? WIFI_AUTH_OPEN : WIFI_AUTH_WPA2_PSK;
+    wificfg.ap.max_connection = 32;
+    wificfg.ap.beacon_interval = 300;
+    strncpy((char*)wificfg.ap.ssid, wifiSsid.c_str(), sizeof(wificfg.ap.ssid) - 1);
+    strncpy((char*)wificfg.ap.password, wifiPw.c_str(), sizeof(wificfg.ap.password) - 1);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wificfg));
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_LOGI(TAG, "wifi_init_softap SSID: %s password: %s", wifiSsid.c_str(), wifiPw.c_str());
 }
@@ -56,11 +64,12 @@ static void wifi_init_sta() {
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler_sta, NULL, NULL));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler_sta_ip, NULL, NULL));
-    wifi_config_t wifi_config = {.sta = {{}, {}, {}, false, {}, 0, 0, WIFI_CONNECT_AP_BY_SIGNAL, {}, {}}};
-    strncpy((char*)wifi_config.sta.ssid, wifiSsid.c_str(), sizeof(wifi_config.sta.ssid) - 1);
-    strncpy((char*)wifi_config.sta.password, wifiPw.c_str(), sizeof(wifi_config.sta.password) - 1);
+    wifi_config_t wificfg = {};
+    wificfg.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
+    strncpy((char*)wificfg.sta.ssid, wifiSsid.c_str(), sizeof(wificfg.sta.ssid) - 1);
+    strncpy((char*)wificfg.sta.password, wifiPw.c_str(), sizeof(wificfg.sta.password) - 1);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wificfg));
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_LOGI(TAG, "wifi_init_sta connecting to SSID: %s", wifiSsid.c_str());
 }
